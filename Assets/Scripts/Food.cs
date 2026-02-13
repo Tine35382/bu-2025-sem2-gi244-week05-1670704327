c35c8918a78e5d3ef520ada1364ad1c9305bca57
Suphasit09 <127718184+Tine35382@users.noreply.github.com>
2026-02-13 16:54:07 +0700

    WIP

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,11 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public int attackPoint = 5;
+    public int attackPoint = 25;
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Hit" +  other.gameObject.name);
+        //var health =  other.gameObject.GetComponent<HealthV1>();
 
+        //if(health != null)
+        //{
+        //    health.TakeDamage(attackPoint);
+        //}
+        if (other.gameObject.TryGetComponent<HealthV1>(out HealthV1 health))
+        {
+            health.TakeDamage(attackPoint);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+
+    public int hitCount = 0;
+    public const string ENEMY_TAG = "Enemy";
+
     void OnTriggerEnter(Collider other)
     {
+        //if (other.gameObject.tag  == "Enemy")
+        //{
+
+        //    hitCount++;
+        //}
+        if (other.gameObject.CompareTag("Enemy") )
+        {
+
+            hitCount++;
+        }
+
+        if (hitCount >= 5000000000000)
+        {
+            Debug.Log("Game OVER");
+            Time.timeScale = 0f;
+            
+        }
 
     }
 }
